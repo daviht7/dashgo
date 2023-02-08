c8f0f6f0ccc36d3eab278f3935aa0f1f552fd3d5
Davi Holanda <daviht7@gmail.com>
2023-02-08 18:33:27 -0300

    pagina signin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,60 @@
-import { Heading } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+} from "@chakra-ui/react";
 
 export default function Home() {
-  return <Heading>olá</Heading>;
+  return (
+    <Flex w="100vw" h="100vh" alignItems={"center"} justifyContent="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing={4}>
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              name="email"
+              id="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant={"filled"}
+              _hover={{
+                bgColor: "gray.900",
+              }}
+              size="lg"
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="email">Password</FormLabel>
+            <Input
+              name="password"
+              id="password"
+              type="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant={"filled"}
+              _hover={{
+                bgColor: "gray.900",
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+        <Button type="submit" marginTop={6} colorScheme="pink">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
