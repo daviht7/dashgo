0dbf4d903f1a0a939038dd9921df3e084e7fdd1c
Davi Holanda <daviht7@gmail.com>
2023-02-14 21:40:30 -0300

    sinalizando refetch dos dados

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -24,7 +24,7 @@ import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { useQuery } from "react-query";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery(
+  const { data, isLoading, error, isFetching } = useQuery(
     "users",
     async () => {
       const response = await fetch("http://localhost:3000/api/users");
@@ -66,6 +66,9 @@ export default function UserList() {
           <Flex mb="8" justifyContent={"space-between"} align="center">
             <Heading size="lg" fontWeight={"normal"}>
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner color="gray.500" size={"sm"} ml="4" />
+              )}
             </Heading>
             <Link href="/users/create" passHref>
               <Button
